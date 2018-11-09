95fd318b4a0f60341fd51e7013554d4cdfbcb656
Katherin-Chuco <katherin.ch.20@gmail.com>
2018-11-09 09:15:38 -0500

    Correciones playlist

--- MovieNight/app/src/main/java/pe/edu/upc/movienight/viewControllers/fragments/LibraryFragment.kt
+++ MovieNight/app/src/main/java/pe/edu/upc/movienight/viewControllers/fragments/LibraryFragment.kt
@@ -1,13 +1,26 @@
 package pe.edu.upc.movienight.viewControllers.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import com.androidnetworking.error.ANError
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 import pe.edu.upc.movienight.R
+import pe.edu.upc.movienight.models.Search
+import pe.edu.upc.movienight.network.OmdbApi
+import pe.edu.upc.movienight.network.SearchResponse
+import pe.edu.upc.movienight.viewControllers.adapters.SearchAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,11 +33,59 @@ private const val ARG_PARAM2 = "param2"
  */
 class LibraryFragment : Fragment() {
 
+    private lateinit var titles: ArrayList<Search>
+    private lateinit var searchRecyclerView: RecyclerView
+    private lateinit var searchAdapter: SearchAdapter
+    private lateinit var searchLayoutManager: RecyclerView.LayoutManager
+    private lateinit var searchEditText: EditText
+    private lateinit var searchButton: ImageButton
+    private lateinit var inm: InputMethodManager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        titles = ArrayList()
+        searchEditText = view.searchEditText
+        searchButton = view.searchButton
+        searchRecyclerView = view.searchRecyclerView
+        searchAdapter = SearchAdapter(titles, view.context)
+        searchLayoutManager = GridLayoutManager(view.context, 2)
+
+        searchRecyclerView.adapter = searchAdapter
+        searchRecyclerView.layoutManager = searchLayoutManager
+
+        inm = context!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+
+        searchButton.setOnClickListener {
+            if(!searchEditText.text.toString().isEmpty()){
+                val text = searchEditText.text.toString()
+                OmdbApi.requestSearch(getString(R.string.omdb_api_key), text,
+                        { response -> responseHandler(response) },
+                        { error -> errorHandler(error) })
+            }
+            searchEditText.clearFocus()
+            //esconder el keyboard
+            inm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        return view
     }
 
+    private fun responseHandler(response: SearchResponse?){
+        /*if(response!!.Search!!.size <= 0){
+            Log.d("MovieNight", "No se encontraron coincidencias")
+            return
+        }*/
+        titles = response!!.Search!!
+        searchAdapter.titles = titles
+        searchAdapter.notifyDataSetChanged()
+    }
+
+    private fun errorHandler(anError: ANError?){
+        Log.d("MovieNight", anError!!.message)
+    }
 
 }
